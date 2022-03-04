e603a731186847ff3527d988f65f9802516912b7
Dube, Brenton <brenton_dube@optum.com>
2022-03-04 11:29:17 -0600

    add start.spring.io project with actuator, starter-web, cloud-stream, rabbit-binder, and lombok

---
+++ src/main/java/com/example/spring/boot/rabbitmq/MessagingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/boot/rabbitmq/MessagingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
